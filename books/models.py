9960e6c66147048ab7c732b64317bacdbdadd78d
VidalYT <vidalgames12345@gmail.com>
2026-04-24 05:20:51 -0700

    We finished and fixed so many bugs. Now the site is working with the templates of the teacher.

--- books/models.py
+++ books/models.py
@@ -1,24 +1,29 @@
 from django.db import models
 
-# New seccion of the code added (To get All URL Generated).
+# Añadimos la seccion Category con los valores que habra en el campo.
 class Category(models.Model):
-    title = models.CharField(max_length=250)
-   
-class Book(models.Model):
+    name = models.CharField(max_length=250)
+    class Meta:
+        verbose_name_plural = "Categories"
+    def __str__(self):
+        return self.name
 
+# Añadimos la seccion Book con los valores que habran en los campos.
+class Book(models.Model):
     ACTIVE = 'active'
     DRAFT = 'draft'
-
-    CHOICES_STATUS = {
+    CHOICES_STATUS = [
         (ACTIVE, 'Active'),
-        (DRAFT, 'Draft')
-    }
+        (DRAFT, 'Draft'),
+    ]
 
-    category = models.ForeignKey(Category, related_name='Book', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='books', on_delete=models.CASCADE)
     title = models.CharField(max_length=250)  
     author = models.CharField(max_length=250)  
     image = models.CharField(max_length=2000)
-
-
+    status = models.CharField(max_length=20, choices=CHOICES_STATUS, default=ACTIVE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
